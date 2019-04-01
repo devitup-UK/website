942772ff1249e8479889e2e46e7e49e288daf0e5
Tom Alderson <me@tomaldy.com>
2019-04-01 21:09:37 +0100

    Banner is now finished, still got the mobile version to do and started on Guarantee section of home page.

---
+++ src/components/guarantee/guarantee.component.js
@@ -0,0 +1,6 @@
+export default {
+    name: 'Guarantee',
+    props: {
+      msg: String
+    }
+  }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <banner></banner>
+    <guarantee></guarantee>
   </div>
 </template>
 
 <script>
 import Banner from '@/components/banner/banner.component.vue'
+import Guarantee from '@/components/guarantee/guarantee.component.vue'
 
 export default {
   name: 'home',
   components: {
-    Banner
+    Banner,
+    Guarantee
   }
 }
 </script>
